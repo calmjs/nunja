1ae779f818eb5642c93f5e5b07f523b1d9b0f4b4
Tommy Yu <tommy.yu@auckland.ac.nz>
2016-11-29 17:05:21 +1300

    Apply the top level dir restrictions
    - So the default test runner will correctly gather the tests.

--- src/nunja/tests/__init__.py
+++ src/nunja/tests/__init__.py
@@ -1,8 +1,11 @@
 import unittest
+from os.path import dirname
 
 
 def make_suite():  # pragma: no cover
     test_loader = unittest.TestLoader()
     test_suite = test_loader.discover(
-        'nunja.tests', pattern='test_*.py')
+        'nunja.tests', pattern='test_*.py',
+        top_level_dir=dirname(__file__),
+    )
     return test_suite
