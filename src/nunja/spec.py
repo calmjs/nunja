c44552d058d1278e1fe529fe471a4c259da3f94c
Tommy Yu <tommy.yu@auckland.ac.nz>
2017-11-17 21:26:23 +1300

    Complete refactor of the nunja precompile process
    - Have the Node.js interfacing nunjucks.precompile be its own function.
    - Create a generalized precompile_nunja function that only does the core
      bits.
    - Special handling of requirejs remains in its own function.
    - The webpack handler simply invoke the generalised version with the
      expected spec keys.

--- src/nunja/spec.py
+++ src/nunja/spec.py
@@ -2,7 +2,7 @@
 import logging
 import re
 
-from codecs import encode
+import codecs
 from collections import defaultdict
 from os.path import dirname
 from os.path import join
@@ -23,140 +23,97 @@ logger = logging.getLogger(__name__)
 
 
 def to_hex(s):
-    return encode(s.encode('utf8'), 'hex_codec').decode('utf8')
+    return codecs.encode(s.encode('utf8'), 'hex_codec').decode('utf8')
 
 
-def precompile_nunja(spec, slim=False):
-    required = {
-        'plugin_sourcepath', 'transpile_sourcepath', 'bundle_sourcepath',
-        BUILD_DIR,
-    }
+def nunjucks_precompile(path, name):
+    require_stmt = 'var nunjucks = require("nunjucks");\n'
+    stdout, stderr = node(
+        '%sprocess.stdout.write(nunjucks.precompile(%s, {"name": %s}));'
+        % (require_stmt, json_dumps(path), json_dumps(name))
+    )
+    if stderr:
+        logger.error("failed to precompile '%s'\n%s'", path, stderr)
+        return None
+    else:
+        return stdout
+
+
+def precompile_nunja(
+        spec, slim,
+        base_sourcepath_key, bundle_sourcepath_key, omit_paths=()):
+    """
+    The generic precompile_nunja function.
+    """
+
+    required = {base_sourcepath_key, bundle_sourcepath_key, BUILD_DIR}
     missing = required.difference(spec.keys())
     if missing:
         raise AdviceAbort(
             'cannot precompile_nunja if spec is missing keys {%s}' % ', '.join(
                 sorted(missing)))
 
-    plugin_sourcepath = spec['plugin_sourcepath']
-    require_stmt = 'var nunjucks = require("nunjucks");\n'
-    standard_sourcepath = {}
+    base_sourcepath = spec[base_sourcepath_key]
+    precompiled_modnames = []
     molds = defaultdict(list)
-    for k, path in plugin_sourcepath.items():
+    slim_bundle_modnames = []
+
+    for modname, path in base_sourcepath.items():
         # could express this more succinctly with regex, probably
-        values = k.split('!', 2)[:2]
-        plugin, name = values
-        parts = name.split('/', 2)
-        if path == EMPTY:
-            continue
-        elif plugin != 'text' and not name.endswith('.nja'):
-            standard_sourcepath[k] = path
-            continue
-        elif len(parts) < 3:
-            # a template with an incompatible naming scheme
-            # TODO should warn about this?
-            standard_sourcepath[k] = path
+        if path in omit_paths:
             continue
 
-        mold_id = '/'.join(parts[:2])
+        match = nunjucks_nja_patt.match(modname)
+        if not match:
+            logger.debug("'%s' is an incompatible nunja template name", path)
+            continue
 
-        # start processing the name
-        stdout, stderr = node(
-            '%sprocess.stdout.write(nunjucks.precompile(%s, {"name": %s}));'
-            % (require_stmt, json_dumps(path), json_dumps(name))
-        )
+        mold = nunjucks_precompile(path, match.group('name'))
+        if mold:
+            molds[match.group('mold_id')].append(mold)
+            precompiled_modnames.append(modname)
 
-        if stderr:
-            logger.error("failed to precompile '%s'\n%s'", path, stderr)
-        else:
-            molds[mold_id].append(stdout)
-
-    if molds:
-        for mold_id, precompiled in molds.items():
-            # use a surrogate name as the bundle process in calmjs will
-            # copy that into the final location.
-            f = join(spec['build_dir'], to_hex(mold_id) + '.js')
-            with open(f, 'w') as fd:
-                for stdout in precompiled:
-                    fd.write(stdout)
-            modname = '/'.join([NUNJA_PRECOMP_NS, mold_id])
-            spec['bundle_sourcepath'][modname] = f
-            spec['shim'] = spec.get('shim', {})
-            spec['shim'][modname] = {
-                'exports': 'nunjucksPrecompiled'
-            }
+    for mold_id, precompiled in molds.items():
+        # use a surrogate name as the bundle process in calmjs will
+        # copy that into the final location.
+        f = join(spec['build_dir'], to_hex(mold_id) + '.js')
+        with codecs.open(f, 'w', encoding='utf8') as fd:
+            for stdout in precompiled:
+                fd.write(stdout)
+        modname = '/'.join([NUNJA_PRECOMP_NS, mold_id])
+        slim_bundle_modnames.append(modname)
+        spec[bundle_sourcepath_key][modname] = f
 
     if slim:
-        spec['plugin_sourcepath'] = standard_sourcepath
-        nunjucks_path = spec['bundle_sourcepath'].get('nunjucks')
-        if nunjucks_path and nunjucks_path != EMPTY:
-            spec['bundle_sourcepath']['nunjucks'] = join(
+        for modname in precompiled_modnames:
+            base_sourcepath.pop(modname)
+        nunjucks_path = spec[bundle_sourcepath_key].get('nunjucks')
+        if nunjucks_path and nunjucks_path not in omit_paths:
+            spec[bundle_sourcepath_key]['nunjucks'] = join(
                 dirname(nunjucks_path), 'nunjucks-slim.js')
+
+    return slim_bundle_modnames,
+
+
+def precompile_nunja_rjs(spec, slim=False):
+    slim_bundle_modnames = precompile_nunja(
+        spec, slim, 'plugin_sourcepath', 'bundle_sourcepath',
+        omit_paths=(EMPTY,)
+    )[0]
+
+    shim = spec['shim'] = spec.get('shim', {})
+    for modname in slim_bundle_modnames:
+        shim[modname] = {'exports': 'nunjucksPrecompiled'}
 
 
 def rjs(spec, extras):
     slim = 'slim' in extras
-    spec.advise(BEFORE_COMPILE, precompile_nunja, spec, slim)
-
-
-def precompile_nunja_webpack(spec, slim=False):
-    required = {
-        'loaderplugin_sourcepath', 'transpile_sourcepath', 'bundle_sourcepath',
-        BUILD_DIR,
-    }
-    missing = required.difference(spec.keys())
-    if missing:
-        raise AdviceAbort(
-            'cannot precompile_nunja if spec is missing keys {%s}' % ', '.join(
-                sorted(missing)))
-
-    require_stmt = 'var nunjucks = require("nunjucks");\n'
-    standard_sourcepath = {}
-    remove = []
-    molds = defaultdict(list)
-    for k, path in spec['loaderplugin_sourcepath'].items():
-        values = k.split('!', 2)[:2]
-        plugin, name = values
-        parts = name.split('/', 2)
-        if plugin != 'text' and not name.endswith('.nja'):
-            # standard_sourcepath[k] = path
-            continue
-        elif len(parts) < 3:
-            # a template with an incompatible naming scheme
-            # TODO should warn about this?
-            # standard_sourcepath[k] = path
-            continue
-
-        mold_id = '/'.join(parts[:2])
-
-        # start processing the name
-        stdout, stderr = node(
-            '%sprocess.stdout.write(nunjucks.precompile(%s, {"name": %s}));'
-            % (require_stmt, json_dumps(path), json_dumps(name))
-        )
-
-        if stderr:
-            logger.error("failed to precompile '%s'\n%s'", path, stderr)
-        else:
-            molds[mold_id].append(stdout)
-
-    if molds:
-        for mold_id, precompiled in molds.items():
-            # use a surrogate name as the bundle process in calmjs will
-            # copy that into the final location.
-            f = join(spec['build_dir'], to_hex(mold_id) + '.js')
-            with open(f, 'w') as fd:
-                for stdout in precompiled:
-                    fd.write(stdout)
-            modname = '/'.join([NUNJA_PRECOMP_NS, mold_id])
-            spec['bundle_sourcepath'][modname] = f
-
-    if slim:
-        nunjucks_path = spec['bundle_sourcepath'].get('nunjucks')
-        if nunjucks_path and nunjucks_path != EMPTY:
-            spec['bundle_sourcepath']['nunjucks'] = join(
-                dirname(nunjucks_path), 'nunjucks-slim.js')
+    spec.advise(BEFORE_COMPILE, precompile_nunja_rjs, spec, slim)
 
 
 def webpack(spec, extras):
     slim = 'slim' in extras
-    spec.advise(BEFORE_COMPILE, precompile_nunja_webpack, spec, slim)
+    spec.advise(
+        BEFORE_COMPILE, precompile_nunja,
+        spec, slim, 'loaderplugin_sourcepath', 'bundle_sourcepath',
+    )
